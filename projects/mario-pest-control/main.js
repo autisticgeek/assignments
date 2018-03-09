dd39b0ec5e86afcc49aa593d09f9e3bb1fe1536a
Steven M Blakely <email@autisticgeek.com>
2018-03-09 10:10:45 -0700

    9 Mar morn

---
+++ projects/mario-pest-control/main.js
@@ -0,0 +1,37 @@
+var data = {};
+var goomba = document.getElementById("goombas");
+var goombaCost = document.getElementById("goombaCost");
+var bobs = document.getElementById("bobombs");
+var bobCost = document.getElementById("bobCost");
+var cheep = document.getElementById("cheepcheeps");
+var cheepCost = document.getElementById("cheepCost");
+var totalCost = document.getElementById("total");
+
+var setGoomba = function (event) {
+    data.goombas = parseInt(event.target.value);
+    goombaCost.value = data.goombas * 5;
+    setTotal();
+}
+var setBob = function (event) {
+    data.bobs = parseInt(event.target.value);
+    bobCost.value = data.bobs * 7;
+    setTotal();
+}
+var setCheep = function (event) {
+    data.cheeps = parseInt(event.target.value);
+    cheepCost.value = data.cheeps * 11;
+    setTotal();
+}
+var setTotal = function () {
+    var goomba = parseInt(goombaCost.value);
+    var bob = parseInt(bobCost.value);
+    var cheep = parseInt(cheepCost.value);
+    totalCost.value = goomba + bob + cheep;
+}
+
+goomba.addEventListener("input", setGoomba);
+bobs.addEventListener("input", setBob);
+cheep.addEventListener("input", setCheep);
+
+
+document.getElementById('url').innerHTML = '<a href="' + window.location.href + '">' + window.location.href + '</a>';
