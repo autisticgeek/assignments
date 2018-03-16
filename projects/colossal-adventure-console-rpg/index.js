486aab51052f20a1f278324492388ee391ea2424
Steven M Blakely <email@autisticgeek.com>
2018-03-16 11:04:11 -0600

    added panther

--- projects/colossal-adventure-console-rpg/index.js
+++ projects/colossal-adventure-console-rpg/index.js
@@ -5,7 +5,7 @@ var initialHitPoints = 100;
 
 
 console.log('\033c')
-var greet = "Greetings travler…\nWelcome to the country of Smalland.\n\nWhat shall I call you?\n\t";
+var greet = "Greetings travler…\nWelcome to the country of UnnamedLand.\n\nWhat shall I call you?\n\t";
 var name = ask.question(greet);
 
 function Player(player) {
@@ -23,7 +23,7 @@ var Enemy = function () {
 }
 
 function getRandomType() {
-    var enemyTypes = ["Ancient Dragon", "Prowler", "Mighty Grunt"];
+    var enemyTypes = ["Ancient Dragon", "Panther", "Mighty Grunt"];
     var randIndex = Math.floor(Math.random() * enemyTypes.length);
     return enemyTypes[randIndex];
 }
@@ -32,7 +32,7 @@ Enemy.prototype.genHitPoints = function (type) {
     switch (type) {
         case "Ancient Dragon":
             return Math.floor(Math.random() * 21) + 80;
-        case "Prowler":
+        case "Panther":
             return Math.floor(Math.random() * 30) + 50;
         case "Mighty Grunt":
             return Math.floor(Math.random() * 30) + 20;
@@ -42,6 +42,30 @@ var enemys = [];
 for (var i = 0; i < numEnemys; i++) {
     enemys.push(new Enemy);
 }
+function panther(){
+var panther = `                         ..,co88oc.oo8888cc,..
+o8o.               ..,o8889689ooo888o"88888888oooc..
+.88888             .o888896888".88888888o'?888888888889ooo....
+a888P          ..c6888969""..,"o888888888o.?8888888888"".ooo8888oo.
+088P        ..atc88889"".,oo8o.86888888888o 88988889",o888888888888.
+888t  ...coo688889"'.ooo88o88b.'86988988889 8688888'o8888896989^888o
+888888888888"..ooo888968888888  "9o688888' "888988 8888868888'o88888
+""G8889""'ooo888888888888889 .d8o9889""'   "8688o."88888988"o888888o .
+         o8888'""""""""""'   o8688"          88868. 888888.68988888"o8o.
+         88888o.              "8888ooo.        '8888. 88888.8898888o"888o.
+         "888888'               "888888'          '""8o"8888.8869888oo8888o .
+    . :.:::::::::::.: .     . :.::::::::.: .   . : ::.:."8888 "888888888888o
+                                                      :..8888,. "88888888888.
+                                                      .:o888.o8o.  "866o9888o
+                                                       :888.o8888.  "88."89".
+                                                      . 89  888888    "88":.
+                                                      :.     '8888o
+                                                       .       "8888..
+                                                                 888888o.
+                                                                  "888889,
+                                                           . : :.:::::::.: :.
+`;
+return console.log(panther);}
 
 
 var dragon = function () {
@@ -86,7 +110,11 @@ var explore = function () {
         switch (enemys[player.enemysKilled-1].type) {
             case 'Ancient Dragon':
                 dragon();
-                console.log(`\nYou found an "Ancient Dragon" and killed it!\n`);
+                console.log(`\nYou found an dragon and killed it!\n`);
+                break;
+                case 'Panther':
+                panther();
+                console.log(`\nYou found a panther and killed it!\n`);
                 break;
         }
 
