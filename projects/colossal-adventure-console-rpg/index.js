e9eda6ee6901c78072021d198b1c2e36f6bbc1c0
Steven M Blakely <email@autisticgeek.com>
2018-03-15 20:21:05 -0600

    Logic work on RPG

--- projects/colossal-adventure-console-rpg/index.js
+++ projects/colossal-adventure-console-rpg/index.js
@@ -5,13 +5,14 @@ var initialHitPoints = 100;
 
 
 console.log('\033c')
-var greet = "Greetings travler…\nWelcome to the country of Smalland.\n\nWhat shall we call you?\n\t";
+var greet = "Greetings travler…\nWelcome to the country of Smalland.\n\nWhat shall I call you?\n\t";
 var name = ask.question(greet);
 
 function Player(player) {
     this.name = player;
     this.hitpoints = initialHitPoints;
     this.enemysKilled = 0;
+    this.turns = 0;
 }
 player = new Player(name);
 
@@ -43,34 +44,47 @@ for (var i = 0; i < numEnemys; i++) {
 }
 
 var quit = function () {
+    console.log('\033c');
     console.log(`\nQUIT\n`);
     player.hitpoints = 0;
 }
 
 var explore = function () {
-    console.log(`\nwalking about…\n`);
-    if (Math.random() > 0.5) {
-        console.log(`\nYou Found an ememy and killed it!\n`);
+    statusBar = `${player.name}\t Life : ${player.hitpoints}\tPOINTS : ${player.enemysKilled}\tTURNS : ${player.turns}`;
+    var directionArray = ["north", "east", "south", "west"];
+    var direction = directionArray[Math.floor(Math.random() * directionArray.length)]
+    player.turns++;
+    console.log('\033c')
+    console.log(statusBar);
+    console.log(underline.join(''))
+    console.log(`\nYou travel ${direction} for a time…\n`);
+    if (Math.random() > 0.50) {
+        console.log(`\nYou found an ememy and killed it!\n`);
         player.enemysKilled++;
-        console.log(player.enemysKilled);
+    } else {
+        console.log(`\nYou find nothing significant.\n`);
     }
+    action = ask.question(`\nWhat do you want to do now?\n\nPrees q to quit\n[ENTER] or [RETURN] to continue`);
+    // require('child_process').spawnSync("read _ ", {
+    //     shell: true,
+    //     stdio: [0, 1, 2]
+    // });
 }
 
-
+console.log('\033c')
+var statusBar = `${player.name}\t Life : ${player.hitpoints}\tPOINTS : ${player.enemysKilled}\tTURNS : ${player.turns}`;
+var underline = ["\u2500", "\u2500", "\u2500", "\u2500", "\u2500", "\u2500", "\u2500", "\u2500", "\u2500", "\u2500", "\u2500", "\u2500", "\u2500", "\u2500", "\u2500"];
+for (var i = 0; i < statusBar.length; i++) {
+    underline.push('\u2500')
+}
+console.log(statusBar);
+console.log(underline.join(''))
+var action = ask.question(`What do you want to do?\nPrees q to quit\n[ENTER] or [RETURN] to continue`);
 while (player.enemysKilled != enemys.length && player.hitpoints >= 1) {
 
-    
-    console.log(`\u2500`);
-    var statusBar = `${player.name}\t Life : ${player.hitpoints}\tPOINTS : ${player.enemysKilled}`
-    console.log(statusBar);
-    var underline=[];
-    for(var i=0;i<statusBar.length;i++){
-        undefined.push('\u2500')
-    }
-    console.log(underline.join(''))
-    
-    
-    var action = ask.question(`What do you want to do?\n\t(E)xplore\n\t(Q)uit\n\t`);
+
+
+
     switch (action) {
         case "q":
             quit();
@@ -84,16 +98,19 @@ while (player.enemysKilled != enemys.length && player.hitpoints >= 1) {
         case "E":
             explore();
             break;
+        default:
+            explore();
     }
-    console.log('\033c')
-    
+
+
+
 
 }
 var outcome;
 if (player.hitpoints < 1) {
-    outcome = `Wat! ${player.name} is now a crispy corpse!\n\n\n\n\n\n\n`
+    outcome = "\033c"+`\nWat! ${player.name} is now a crispy corpse!\n\n\n\n\n\n\n`
 }
 if (player.enemysKilled === enemys.length) {
-    outcome = `You killed them all!  Someone call the ASPCA!\n\n\n\n\n\n\n`
+    outcome = "\033c\nYou killed them all!  Someone call the ASPCA!\n\n\n\n\n\n\n"
 }
 console.log(outcome);
