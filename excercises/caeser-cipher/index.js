9b9357a62ec030b86a7caf77a6e1f3e5aa87a189
Steven M Blakely <email@autisticgeek.com>
2018-03-20 17:31:14 -0600

    Removed unessary varables

--- excercises/caeser-cipher/index.js
+++ excercises/caeser-cipher/index.js
@@ -1,7 +1,7 @@
 var ask = require("readline-sync");
 var caeserShift = function (str, num) {
     var data = "";
-    var alpfa = "abcdefghijklmnopqrstuvwxyz".split('');
+   // var alpfa = "abcdefghijklmnopqrstuvwxyz".split('');
 
     for (var i = 0; i < str.length; i++) {
         var c
@@ -21,6 +21,5 @@ var caeserShift = function (str, num) {
 }
 var input = ask.question('\nWhat phrase would you like to encrypt?\n').toLowerCase();  
 var shift = parseInt(ask.question('\nHow many letters would you like to shift?\n')); 
-// var input = "hi there";
-// var shift = 15;
+
 console.log(caeserShift(input, shift))
