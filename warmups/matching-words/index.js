4d2a6be6916c3df1c9728b9235554f700c2d6eb6
Steven M Blakely <email@autisticgeek.com>
2018-03-26 10:27:59 -0600

    matching-words

---
+++ warmups/matching-words/index.js
@@ -0,0 +1,18 @@
+let str = "Banh mi pull skateboard ipsum lorem, kombucha scenester banjo. Franzen mlkshk consequat, PBR&B lever lever listicle irony pop-up sriracha on the saliva. Shabby on chic eu iceland far next level far lever pull drinking the right vinegar fanny pack pull minim right chicharrones migas."
+
+
+const matchWords = str => {
+    const counts = {}; //store words we have seen
+    const output = []; //store words that are duplicates
+    str.replace(/[^a-z\s]/gi, "").toLowerCase().split(" ").forEach(str => { //replace that wich is not a letter or a space, convert to all lowercaase, devide it into an array, fun a function on eachb part of the array
+        if (counts.hasOwnProperty(str)) { //check to see if we have seen this word before
+            if (!output.includes(str)) { //check to see of word is NOT already in the output array
+                output.push(str);// adds word to the output array 
+            }
+        } else {
+            counts[str] = true; //adds word to counts object
+        }
+    })
+    return output; //return the output array
+}
+console.log(matchWords(str));
