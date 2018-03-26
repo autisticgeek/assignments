87076830e6d36fb518fb8503b44d188e5288f147
Steven M Blakely <email@autisticgeek.com>
2018-03-26 10:37:42 -0600

    minor changes to a warmup

--- warmups/matching-words/index.js
+++ warmups/matching-words/index.js
@@ -4,13 +4,13 @@ let str = "Banh mi pull skateboard ipsum lorem, kombucha scenester banjo. Franze
 const matchWords = str => {
     const counts = {}; //store words we have seen
     const output = []; //store words that are duplicates
-    str.replace(/[^a-z\s]/gi, "").toLowerCase().split(" ").forEach(str => { //replace that wich is not a letter or a space, convert to all lowercaase, devide it into an array, fun a function on eachb part of the array
+    str.replace(/[^a-z\s]/gi, "").toLowerCase().split(" ").forEach(str => { //replace that wich is not a letter or a space, convert to all lowercaase, devide it into an array, run a function on each part of the array
         if (counts.hasOwnProperty(str)) { //check to see if we have seen this word before
-            if (!output.includes(str)) { //check to see of word is NOT already in the output array
+            if (!output.includes(str)) { //check to see if word is NOT already in the output array
                 output.push(str);// adds word to the output array 
             }
         } else {
-            counts[str] = true; //adds word to counts object
+            counts[str] = " "; //adds word to counts object
         }
     })
     return output; //return the output array
