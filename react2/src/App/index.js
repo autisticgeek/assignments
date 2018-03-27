9dae1c320dc8d0f10c17e14d2176eb268fdcfff7
Steven M Blakely <email@autisticgeek.com>
2018-03-27 12:51:40 -0600

    react 2

---
+++ react2/src/App/index.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+function App() {
+    return (
+        <div style={{ display: "flex" }}>
+            <h1>Hello</h1>
+            <h1>World</h1>
+        </div>
+    )
+}
+export default App
