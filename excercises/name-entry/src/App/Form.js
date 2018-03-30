4ff49eb44c4e0e269917714cc037ee4e90eff34f
Steven M Blakely <email@autisticgeek.com>
2018-03-30 15:26:28 -0600

    name-entry

---
+++ excercises/name-entry/src/App/Form.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import List from "./List"
+
+export default class Form extends Component {
+    constructor(props) {
+        super(props);
+        this.startState={
+            storedInput:""
+            }
+        this.state = {
+            storedInput: "",
+            list: []
+        }
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
+    handleChange(event) {
+        const { value, name } = event.target;
+        this.setState(currentState => {
+            return {
+                ...currentState,
+                [name]: value
+            }
+        })
+    }
+    handleSubmit(event) {
+        event.preventDefault();
+;        this.setState(currentState => {
+            return {
+                storedInput:'',
+                list: [...currentState.list,
+                currentState.storedInput
+                ]
+            }
+
+        }
+        )
+    }
+    render() {
+        const { storedInput, list } = this.state;
+        const printList= list.map((element,index) => <List key={index} content={element}/>)
+        return (
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <input type="text" name="storedInput"
+                        value={storedInput} onChange={this.handleChange} />
+                </form>
+                <h1>{storedInput}</h1>
+                <ul>
+                    {printList}
+                    </ul>
+            </div>
+        )
+    }
+
+}
+
---
+++ excercises/name-entry/src/App/List.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+export default function List(props) {
+    const{content}= props
+    return (
+        <li>{content}</li>
+    )
+}
