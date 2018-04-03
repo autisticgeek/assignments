28d6c7df5248ad6be5cd8c6b1c3d3ef4bc95fffa
Steven M Blakely <email@autisticgeek.com>
2018-04-03 09:58:53 -0600

    Balance

---
+++ warmups/balance-parenthesis/index.js
@@ -0,0 +1,17 @@
+const chkBal = (str) => {
+    let tranquility = 0
+    str.split('').map(element => {
+        if (tranquility < 1)
+            element === "(" ? tranquility-- : tranquility++
+        else
+            return false
+    })
+    return tranquility === 0;
+}
+
+
+console.log(`"()()" // ->`, chkBal("()()"))
+console.log(`"(())" // ->`, chkBal("(())"))
+console.log(`"()))" // ->`, chkBal("()))"))
+console.log(`")()(" // ->`, chkBal(")()("))
+console.log(`"())(" // ->`, chkBal("())("))
