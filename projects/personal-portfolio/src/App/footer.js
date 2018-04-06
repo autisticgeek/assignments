6b92cad73b74c468c506e6d904cb9ce763ccd998
Steven M Blakely <email@autisticgeek.com>
2018-04-06 09:17:20 -0600

    Personal profie work

--- projects/personal-portfolio/src/App/footer.js
+++ projects/personal-portfolio/src/App/footer.js
@@ -1,13 +1,29 @@
 import React from "react";
-import { Link } from 'react-router-dom'
+//import { Link } from 'react-router-dom';
+import axios from "axios"
 
-export default function App() {
-    return (
-        <footer>
-            <div className="container flex-around">
-                <div>&copy; Steven Blakely</div>
-                <div><a href="">EMAIL</a></div>
-            </div>
-        </footer>
-    )
+let clientCity = null;
+export default class Footer extends React.Component {
+    // constructor(props) {
+    //     super(props);
+
+    // }
+
+    componentDidMount() {
+        axios.get('https://ipapi.co/json').then(response => {
+            console.log(response.data)
+            clientCity = response.data.city;
+        })
+    }
+    render() {
+        return (
+            <footer>
+                <div className="container flex-around">
+                    <div>&copy; Steven Blakely</div>
+                    <div><a href="">EMAIL</a></div>
+                    <div>{clientCity}</div>
+                </div>
+            </footer>
+        )
+    }
 }
